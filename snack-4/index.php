b5f4e94b75287fdc1ba87193abb7723365fdb74a
MauroDeStefano <maurodestefano@gmail.com>
2021-12-20 15:08:23 +0100

    snack 4 done

---
+++ snack-4/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+function getRandomArray($min, $max, $nItems){
+
+$newArray = [];
+
+if(($max - $min) < $nItems){
+  $nItems = $max - $min;
+};
+
+while(count($newArray) <  $nItems){
+  $number = rand($min,$max);
+  if(!in_array($number,$newArray)){
+    $newArray[] = $number;
+  }
+
+};
+
+return $newArray;
+}
+
+var_dump(getRandomArray(1, 100, 15));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 4</title>
+</head>
+<body>
+  
+</body>
+</html>
